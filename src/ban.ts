fc90a75df0be0e342ec9be209673212af7d839f5
Saro Amirkhanyan <37337234+saroamirkhanyan@users.noreply.github.com>
2022-06-19 09:53:42 -0700

    Update ban.ts

--- src/ban.ts
+++ src/ban.ts
@@ -9,7 +9,6 @@ function baneval(source: string) {
   const tokens = tokenize(source);
   const program = parse(tokens); 
   const compiledProgram = compile(program);
-	console.log(compiledProgram);
   eval(compiledProgram);
 }
 
